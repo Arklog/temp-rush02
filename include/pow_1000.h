3c0edaa2824ec2f0abc14cf25ac25c64adb6cbf5
Pierre DUCLOUX <pducloux@e3r7p1.clusters.42paris.fr>
2022-07-23 18:04:19 +0200

    modified doc

--- include/pow_1000.h
+++ include/pow_1000.h
@@ -6,7 +6,7 @@
 /*   By: pducloux <pducloux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/23 17:42:03 by pducloux          #+#    #+#             */
-/*   Updated: 2022/07/23 18:00:59 by pducloux         ###   ########.fr       */
+/*   Updated: 2022/07/23 18:04:03 by pducloux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	add_pow_1000(char *str);
  *
  * @param pow The power of 1000
  *
- * @return the corresponding power of 1000 as a string (1 should return 1000)
+ * @return the corresponding power of 1000 as a string (1 should return "thousand")
  */
 char	*get_pow_1000(t_size pow);
 
